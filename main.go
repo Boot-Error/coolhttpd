f0bb15f9787803fa5c9c1bdede387e7294e5d731
Boot-Error <booterror99@gmail.com>
2019-03-12 16:11:13 +0530

    Basic features implemented

--- main.go
+++ main.go
@@ -15,7 +15,9 @@ import (
 	"net/http"
 	"os"
 	"path"
+	"strconv"
 	"strings"
+	"syscall"
 	"time"
 )
 
@@ -35,6 +37,42 @@ type File struct {
 	Modified string `json:"modified"`
 	Size     int64  `json:"size"`
 	Contents []File `json:"contents"`
+}
+
+func colorCodePadding(str string, fgNo int, bgNo int) (ColoredCode string) {
+
+	fgCodes := map[int]string{
+		0: "[30m",
+		1: "[31m",
+		2: "[32m",
+		3: "[33m",
+		4: "[34m",
+		5: "[35m",
+		6: "[36m",
+		7: "[37m",
+		9: "[39m",
+	}
+
+	bgCodes := map[int]string{
+		0: "[40m",
+		1: "[41m",
+		2: "[42m",
+		3: "[43m",
+		4: "[44m",
+		5: "[45m",
+		6: "[46m",
+		7: "[47m",
+		9: "[49m",
+	}
+
+	ColoredCode = fmt.Sprintf(
+		"\033%s\033%s%s\033[30m\033[40m",
+		bgCodes[bgNo],
+		fgCodes[fgNo],
+		str,
+	)
+
+	return
 }
 
 func getDirList(rootdir string) (f File, err error) {
@@ -75,8 +113,8 @@ func renderDirectory(f File, w http.ResponseWriter) {
 		if cf.IsDir {
 			renderDirectory(cf, w)
 		} else {
-			resourceURL := path.Join("/file", cf.Path, cf.Name)
-			fmt.Fprintf(w, "<p><a href=\"%s\" style=\"margin: 0; margin-left:10px\">%s</a></p>", resourceURL, cf.Name)
+			resourceURL := path.Join("/file/", strings.TrimPrefix(cf.Path, config.Directory))
+			fmt.Fprintf(w, "<p style=\"margin: 0px;\"><a href=\"%s\" style=\" margin-left:10px\">%s</a></p>", resourceURL, cf.Name)
 		}
 	}
 	fmt.Fprintf(w, "</details>")
@@ -85,7 +123,13 @@ func renderDirectory(f File, w http.ResponseWriter) {
 
 func directoryListingHandler(w http.ResponseWriter, r *http.Request) {
 
-	f, err := getDirList("/home/booterror/Documents")
+	var err error
+	var f File
+	if config.Chroot {
+		f, err = getDirList("/")
+	} else {
+		f, err = getDirList(config.Directory)
+	}
 	w.Header().Add("Content-Type", "text/html; charset=utf-8")
 
 	if err != nil {
@@ -106,7 +150,8 @@ func directoryListingHandler(w http.ResponseWriter, r *http.Request) {
 
 func fileServingHandler(w http.ResponseWriter, r *http.Request) {
 
-	http.ServeFile(w, r, r.URL.Path)
+	// fmt.Printf("Serving file %s\n", path.Join(config.Directory, strings.TrimPrefix(r.URL.Path, "/file/")))
+	http.ServeFile(w, r, path.Join(config.Directory, strings.TrimPrefix(r.URL.Path, "/file/")))
 }
 
 func AuthMiddleware(handlerFunc http.HandlerFunc) http.HandlerFunc {
@@ -125,7 +170,7 @@ func AuthMiddleware(handlerFunc http.HandlerFunc) http.HandlerFunc {
 			decodedAuthData, err := base64.StdEncoding.DecodeString(authData[1])
 			if err != nil {
 				fmt.Println(err)
-				http.Error(w, "Unauthorized", http.StatusUnauthorized)
+				http.Error(w, "Unauthorized, Failed to decode auth credentials", http.StatusUnauthorized)
 				return
 			}
 			authValues := strings.Split(string(decodedAuthData), ":")
@@ -133,44 +178,80 @@ func AuthMiddleware(handlerFunc http.HandlerFunc) http.HandlerFunc {
 				handlerFunc(w, r)
 			} else {
 				fmt.Println(authValues)
-				http.Error(w, "Unauthorized", http.StatusUnauthorized)
+				http.Error(w, "Unauthorized, wrong credentials", http.StatusUnauthorized)
 				return
 			}
 		} else {
 			fmt.Println(authData[0])
-			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			http.Error(w, "Unauthorized, Couldn't understand auth header", http.StatusUnauthorized)
 			return
 		}
 	}
+}
 
+func LoggingMiddleware(handlerFunc http.HandlerFunc) http.HandlerFunc {
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		handlerFunc(w, r)
+		fmt.Println(
+			r.RemoteAddr,
+			r.Method,
+			r.Proto,
+			r.Header["User-Agent"],
+			r.URL,
+		)
+	}
 }
 
 var config = Config{
 	Chroot:    false,
 	Username:  "test",
 	Passwd:    "123",
-	Directory: "",
+	Directory: ".",
 	Host:      "0.0.0.0",
 	Port:      8000,
 }
 
 func setupConfig() {
 
-	flag.BoolVar(&config.Chroot, "chroot", false, "Locks the server into 2")
-	flag.StringVar(&config.Username, "username", "", "Username for the Basic Authentication")
-	flag.StringVar(&config.Passwd, "passwd", "", "Password for the Basc Authentication")
-	flag.StringVar(&config.Host, "host", "localhost", "ip address for the server")
-	flag.UintVar(&config.Port, "port", 8080, "server port")
+	flag.BoolVar(&config.Chroot, "chroot", config.Chroot, "Locks the server into chroot")
+	flag.StringVar(&config.Username, "username", config.Username, "Username for the Basic Authentication")
+	flag.StringVar(&config.Passwd, "passwd", config.Passwd, "Password for the Basc Authentication")
+	flag.StringVar(&config.Host, "host", config.Host, "ip address for the server")
+	flag.UintVar(&config.Port, "port", config.Port, "server port")
+	flag.StringVar(&config.Directory, "dir", config.Directory, "directory to host")
 
 	flag.Parse()
+}
+
+func setupChroot() {
+
+	var err error
+
+	if !config.Chroot {
+		return
+	}
+
+	err = syscall.Chdir(config.Directory)
+	if err != nil {
+		panic(err)
+	}
+
+	err = syscall.Chroot(config.Directory)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Chrooted to ", config.Directory)
 }
 
 func main() {
 
 	setupConfig()
+	setupChroot()
 
-	http.Handle("/", AuthMiddleware(directoryListingHandler))
-	http.Handle("/file/", AuthMiddleware(fileServingHandler))
+	http.Handle("/", LoggingMiddleware(AuthMiddleware(directoryListingHandler)))
+	http.Handle("/file/", LoggingMiddleware(AuthMiddleware(fileServingHandler)))
 
-	http.ListenAndServe(strings.Join(), nil)
+	http.ListenAndServe(strings.Join([]string{config.Host, strconv.Itoa(int(config.Port))}, ":"), nil)
 }
